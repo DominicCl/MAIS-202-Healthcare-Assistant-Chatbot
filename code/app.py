8a58cde444e4ce8fd770a1604a6f6e8448b046d0
Félix Martel <fmartel2507@gmail.com>
2025-11-24 15:40:26 -0500

    added some code to render the website

--- code/app.py
+++ code/app.py
@@ -128,5 +128,6 @@ def index():
 
 
 if __name__ == "__main__":
-    print(" Running Medical Diagnosis Backend (NN Mode)")
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0", port=port)
